458e029bfbfe71d6e0acee6d477fef6a47b3f281
John Youn <johnyoun@synopsys.com>
2016-07-11 13:24:24 -0700

    Add the Synopsys AXS10x w/DWC_usb3 IP platform
    The axs10x-usb3 platform is similar to the axs10x except that a USB 3.x
    IP is mapped over the tunnel. There are several variants of this based
    on the configuration of the USB 3.x IP.
    
    Signed-off-by: John Youn <johnyoun@synopsys.com>

---
+++ package/usb-scripts/usb-scripts.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# USB_SCRIPTS
+#
+################################################################################
+
+USB_SCRIPTS_VERSION = master
+USB_SCRIPTS_SITE = http://nanobot/dwc/usb-scripts.git
+USB_SCRIPTS_SITE_METHOD = git
+USB_SCRIPTS_INSTALL_TARGET = YES
+USB_SCRIPTS_LICENSE = GPLv2+
+USB_SCRIPTS_LICENSE_FILES = COPYING
+
+define USB_SCRIPTS_BUILD_CMDS
+	$(MAKE) CC=$(TARGET_CROSS)gcc LD=$(TARGET_LD) -C $(@D)
+endef
+
+define USB_SCRIPTS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) prefix=$(TARGET_DIR)/root libdir=/root/dwc_utils install
+endef
+
+$(eval $(generic-package))
