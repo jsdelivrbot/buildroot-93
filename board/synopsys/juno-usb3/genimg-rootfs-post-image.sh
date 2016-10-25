af164569afbf206ab730585ee40de3c2b4c0bc75
Thinh Nguyen <thinhn@synopsys.com>
2016-10-25 11:10:22 -0700

    juno: Generate rootfs as an image
    Create post-image script to use ext4 rootfs output from buildroot
    and generate an image for rootfs.
    
    Change-Id: I822b0f746117acb745f0890c1d7587b7898e358a
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>

---
+++ board/synopsys/juno-usb3/genimg-rootfs-post-image.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+set -e
+
+# Create an image with a partition containing the rootfs
+
+GENIMAGE_ROOTFS_CFG="board/synopsys/juno-usb3/genimage_rootfs.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Clean previous build
+rm -rf $GENIMAGE_TMP
+
+# Create image for rootfs
+genimage \
+    --rootpath $TARGET_DIR \
+    --tmppath $GENIMAGE_TMP \
+    --inputpath $BINARIES_DIR \
+    --outputpath $BINARIES_DIR \
+    --config $GENIMAGE_ROOTFS_CFG
+
+rm -f $BINARIES_DIR/rootfs.ext2
+rm -f $BINARIES_DIR/rootfs.ext4
+gzip -f $BINARIES_DIR/rootfs.img
